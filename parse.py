ac3e96ad77e1da5b534b2d8c63c4a70ffbe3f090
jal347 <linjason.03@gmail.com>
2023-06-01 10:08:11 -0700

    renamed file in parse and uncompess gz file in manifest

--- parse.py
+++ parse.py
@@ -1,15 +1,16 @@
 import os
+
 from biothings.utils.dataload import tabfile_feeder
 
 
 def load_data(data_folder):
-    json_path = os.path.join(data_folder, "significant.csv")
-    records = tabfile_feeder(json_path, sep=',')
-    doc_keys = ['loc','lin','snr','growing','sig']
+    json_path = os.path.join(data_folder, "gr_sigs.csv")
+    records = tabfile_feeder(json_path, sep=",")
+    doc_keys = ["loc", "lin", "snr", "growing", "sig"]
     for record in records:
         doc = {}
         for i, key in enumerate(doc_keys):
-            if key == 'growing':
+            if key == "growing":
                 doc[key] = record[i].lower()
             else:
                 doc[key] = record[i]
@@ -19,19 +20,9 @@ def load_data(data_folder):
 
 def custom_data_mapping(cls):
     return {
-        "growing": {
-            "type": "boolean"
-        },
-        "lin": {
-            "type": "keyword"
-        },
-        "loc": {
-            "type": "keyword"
-        },
-        "sig": {
-            "type": "double"
-        },
-        "snr": {
-            "type": "double"
-        }
+        "growing": {"type": "boolean"},
+        "lin": {"type": "keyword"},
+        "loc": {"type": "keyword"},
+        "sig": {"type": "double"},
+        "snr": {"type": "double"},
     }
