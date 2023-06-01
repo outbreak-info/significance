9637b812cc469af6ab923dea9bd2d5227f7e2910
jal347 <linjason.03@gmail.com>
2023-06-01 10:02:27 -0700

    fixed versioning

--- version.py
+++ version.py
@@ -1,10 +1,5 @@
 def get_release(self):
     import requests
-    from xml.etree import ElementTree as ET
-    res = requests.get('http://storage.googleapis.com/andersen-lab_temp').content
-    root = ET.fromstring(res)
-    # we cannot use text() to search b/c xml does not support text()
-    # https://stackoverflow.com/questions/33830821/python-xpath-syntaxerror-invalid-predicate
-    # .//{http://doc.s3.amazonaws.com/2006-03-01}Key[text()='outbreak_info/significant.csv']/following-sibling::{http://doc.s3.amazonaws.com/2006-03-01}LastModified
-    last_modified = root.find(".//{http://doc.s3.amazonaws.com/2006-03-01}Contents[{http://doc.s3.amazonaws.com/2006-03-01}Key='outbreak_info/significant.csv']/{http://doc.s3.amazonaws.com/2006-03-01}LastModified")
-    return last_modified.text
+
+    res = requests.head("http://su13/outbreak/significance/gr_sigs.csv.gz")
+    return res.headers["Last-Modified"]
