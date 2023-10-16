6c7a04f68f3438eb163dc910999d065587b84cde
Everaldo <everaldorodrigo@gmail.com>
2023-10-16 16:29:24 -0700

    Add leaf field

--- parse.py
+++ parse.py
@@ -6,11 +6,11 @@ from biothings.utils.dataload import tabfile_feeder
 def load_data(data_folder):
     json_path = os.path.join(data_folder, "gr_sigs.csv")
     records = tabfile_feeder(json_path, sep=",")
-    doc_keys = ["loc", "lin", "snr", "growing", "sig"]
+    doc_keys = ["loc", "lin", "leaf", "snr", "growing", "sig"]
     for record in records:
         doc = {}
         for i, key in enumerate(doc_keys):
-            if key == "growing":
+            if key in ["growing", "leaf"]:
                 doc[key] = record[i].lower()
             else:
                 doc[key] = record[i]
@@ -21,6 +21,7 @@ def load_data(data_folder):
 def custom_data_mapping(cls):
     return {
         "growing": {"type": "boolean"},
+        "leaf": {"type": "boolean"},
         "lin": {"type": "keyword"},
         "loc": {"type": "keyword"},
         "sig": {"type": "double"},
